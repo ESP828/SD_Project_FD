69a5da9742d6ac03329a5b70e3042ec5affbb394
Shiori <97660517+paikyeon@users.noreply.github.com>
2026-07-30 17:23:53 +0900

    Revert "restaurant_SJ"
    This reverts commit 441f255345e4ae79db2edff500332f25595690dd.

--- springboot/src/main/java/com/example/backend/restaurant/domain/entity/OwnerProfile.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.restaurant.domain.entity;
-
-public class OwnerProfile {
-  
-}
--- springboot/src/main/java/com/example/backend/restaurant/domain/type/RestaurantStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.restaurant.domain.type;
-
-public enum RestaurantStatus {
-    ACTIVE,
-    INACTIVE,
-    DELETED
-}
