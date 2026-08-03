e481884d1bb05a91a1d1ad1e55a95baa030aaa12
ila98111763-bit <ila98111763@gmail.com>
2026-08-03 14:45:19 +0900

    홈페이지 배너 '이미지' 수정& 맛집찾기 푸터 지우기_윤서영
    9시- 12시 50분까지 한거 올림.

--- springboot/src/main/java/com/example/backend/restaurant/domain/entity/Restaurant.java
+++ springboot/src/main/java/com/example/backend/restaurant/domain/entity/Restaurant.java
@@ -49,7 +49,11 @@ public class Restaurant {
     @Column(name = "closed_days", length = 255)
     private String closedDays;
 
+<<<<<<< Updated upstream
     @Column(columnDefinition = "TEXT")
+=======
+    @Column(length = 65535)
+>>>>>>> Stashed changes
     private String description;
 
     @Enumerated(EnumType.STRING)
