bd0d2fccbb68dc351fb5729d86bc6fab5e9a5d74
ssbgit01 <ssbo0621@gmail.com>
2026-08-04 09:11:43 +0900

    new post 부분 전부 삭제

--- springboot/src/main/java/com/example/backend/news/repository/NewsPostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.news.repository;
-
-import com.example.backend.news.domain.entity.NewsPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsPostRepository extends JpaRepository<NewsPost, Long> {
-}
