ac1eb1dbe189eac56a9dfbc527aebaa935e88826
ssbgit01 <ssbo0621@gmail.com>
2026-08-20 16:55:03 +0900

    import java.util.List 추가
    추가

--- springboot/src/main/java/com/example/backend/review/repository/ReviewRepository.java
+++ springboot/src/main/java/com/example/backend/review/repository/ReviewRepository.java
@@ -1,13 +1,15 @@
 package com.example.backend.review.repository;
 
-import com.example.backend.review.domain.entity.Review;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import com.example.backend.review.domain.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
